f42720b8fe3d13e00368532cc21ad1c7f3abccef
cc54321 <charles.cole@codeimmersives.com>
2023-03-10 20:48:49 -0500

    commit -a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import BasicExample from './components/Rows';
+import { useEffect, useState } from 'react';
+
+
+const DATA_URL_RICK = "https://rickandmortyapi.com/api/character"
+
+const DATA_URL_MORTY = "https://rickandmortyapi.com/api/character/?page=2"
+
 
 function App() {
+
+const [cardData, updateCards] = useState([])
+const [pageData, updatePage] = useState([])
+
+  useEffect(() => {
+
+  
+    
+    // const moviesData = []
+    fetch(DATA_URL_RICK)
+    .then((result) => result.json())
+    .then((result) => {
+      updateCards(result.results)
+
+    });
+  
+    }, [])
+
+
+    useEffect(() => {
+
+   
+      // const moviesData = []
+      fetch(DATA_URL_MORTY)
+      .then((result) => result.json())
+      .then((result) => {
+        updatePage(result.results)
+  
+      });
+    
+      }, [])
+
+      
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <BasicExample data = {cardData} page = {pageData}/>
+
+      
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Rows.js
@@ -0,0 +1,73 @@
+import { Component } from "react";
+import { Container, Row, Card, Tab, Tabs } from "react-bootstrap";
+import Flip from "./Flip";
+import Collapsed from "./Collapse";
+
+
+
+//page 1
+const RickData = (props) => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          {props.data.map((characters) => {
+            return <Flip characters={characters} />;
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+//page 2
+const MortyData = (props) => {
+ 
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          {props.page.map((characters) => {
+            return <Collapsed characters={characters} />;
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+
+class BasicExample extends Component {
+  render() {
+    const { data, page } = this.props;
+    return (
+      <div>
+        <Tabs
+          defaultActiveKey="Page1"
+          id="uncontrolled-tab-example"
+          className="mb-3"
+        >
+          <Tab eventKey="Page1" title="Page1">
+            <Card className="card">
+              <Card.Header>
+                <h1>Rick and Morty Characters</h1>
+              </Card.Header>
+              <RickData data={data} />
+            </Card>
+          </Tab>
+          <Tab eventKey="page2" title="Page2">
+            <Card className="card">
+              <Card.Header>
+                <h1>Rick and Morty Characters</h1>
+              </Card.Header>
+              <MortyData page={page} />
+            </Card>
+          </Tab>
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+export default BasicExample;
