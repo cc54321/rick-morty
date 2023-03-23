7ef73379033185ec2ede55fcb28c58248d537679
cc54321 <charles.cole@codeimmersives.com>
2023-03-22 22:25:21 -0400

    commit -a

--- src/App.js
+++ src/App.js
@@ -1,58 +1,51 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import BasicExample from './components/Rows';
-import { useEffect, useState } from 'react';
-
-
-const DATA_URL_RICK = "https://rickandmortyapi.com/api/character"
-
-const DATA_URL_MORTY = "https://rickandmortyapi.com/api/character/?page=2"
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-
-const [cardData, updateCards] = useState([])
-const [pageData, updatePage] = useState([])
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-
-  
-    
-    // const moviesData = []
-    fetch(DATA_URL_RICK)
-    .then((result) => result.json())
-    .then((result) => {
-      updateCards(result.results)
-
-    });
-  
-    }, [])
-
-
-    useEffect(() => {
-
-   
-      // const moviesData = []
-      fetch(DATA_URL_MORTY)
-      .then((result) => result.json())
-      .then((result) => {
-        updatePage(result.results)
-  
-      });
-    
-      }, [])
-
-      
-
+    async function fetchData() {
+      const response = await axios.get(
+        "https://rickandmortyapi.com/api/character/?page=1"
+      );
+      setCharacters(response.data.results);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      
-      <BasicExample data = {cardData} page = {pageData}/>
-
-      
+      <h1>Rick and Morty Characters</h1>
+      <div className="flip-card-container">
+        {characters.slice(0, 60).map((character) => (
+          <div className="flip-card" key={character.id}>
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img src={character.image} alt={character.name} />
+                <h2>{character.name}</h2>
+              </div>
+              <div className="flip-card-back">
+                <h3>{character.name}</h3>
+                <p>Status: {character.status}</p>
+                <p>Species: {character.species}</p>
+                <p>Gender: {character.gender}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
 
+
+
+
+
+
+
+
+ 
